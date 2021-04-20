4dc596adc657c4a02b962f9590a811bc4fdcc73d
coolabhays <abhaysp9955@gmail.com>
2021-04-20 10:24:55 +0530

    show demo with data constructed with rvalues

--- tigen/src/main.cpp
+++ tigen/src/main.cpp
@@ -22,20 +22,30 @@ int main(int argc, char **argv) {
 	entities::class_time ct1("ct1", "9:00 - 10:00");
 	entities::class_time ct2("ct2", "12:00 - 13:00");
 
-	std::vector<entities::instructor> iset1 = {i1, i2};
-	std::vector<entities::instructor> iset2 = {i2, i3};
-	std::vector<entities::instructor> iset3 = {i3, i4};
-	entities::course c1("c1", "kyu", 50, iset1);
-	entities::course c2("c2", "kaise", 42, iset2);
-	entities::course c3("c3", "kisliye", 54, iset3);
+	/* {  // using lvalues
+		std::vector<entities::instructor> iset1 = {i1, i2};
+		std::vector<entities::instructor> iset2 = {i2, i3};
+		std::vector<entities::instructor> iset3 = {i3, i4};
+		entities::course c1("c1", "kyu", 50, iset1);
+		entities::course c2("c2", "kaise", 42, iset2);
+		entities::course c3("c3", "kisliye", 54, iset3);
 
-	// make this type of constructor too
-	//entities::course c1("c1", "kyu", 50, std::vector<entities::instructor>(i1, i2));
+		// make this type of constructor too
+		//entities::course c1("c1", "kyu", 50, std::vector<entities::instructor>(i1, i2));
 
-	std::vector<entities::course> dset1 = {c1};
-	std::vector<entities::course> dset2 = {c2, c3};
-	entities::department d1("kharab hai", dset1);
-	entities::department d2("ye bhi kharab hai", dset2);
+		std::vector<entities::course> dset1 = {c1};
+		std::vector<entities::course> dset2 = {c2, c3};
+		entities::department d1("kharab hai", dset1);
+		entities::department d2("ye bhi kharab hai", dset2);
+	} */
+
+	// using rvalues
+	entities::course c1("c1", "kyu", 50, std::vector<entities::instructor>{i1, i2});
+	entities::course c2("c2", "kaise", 42, std::vector<entities::instructor>{i2, i3});
+	entities::course c3("c3", "kisliye", 54, std::vector<entities::instructor>{i3, i4});
+
+	entities::department d1("kharab hai", std::vector<entities::course>{c1});
+	entities::department d2("ye bhi kharab hai", std::vector<entities::course>{c2, c3});
 
 	std::vector<entities::department> all_depts = {d1, d2};
 
@@ -43,12 +53,16 @@ int main(int argc, char **argv) {
 		std::cout << "Department: " << dept.get_name() << ", ";
 		for (const entities::course& crs: dept.get_courses()) {
 			std::cout << "Course: " << crs.get_name() << ", ";
-			for (const entities::instructor& inst: crs.get_instructors()) {
-				std::cout << "inst: " << inst.get_name() << "\n";
+			std::vector<entities::instructor> cur_inst = crs.get_instructors();
+			for (std::vector<entities::instructor>::const_iterator citer = cur_inst.cbegin();
+					citer != cur_inst.cend(); ++citer) {
+				std::cout << "inst: " << citer->get_name();
+				if (citer < cur_inst.cend() - 1) {
+					std::cout << ", ";
+				}
 			}
+			std::cout << std::endl;
 		}
 	}
-
-	std::cout << std::endl;
 	return 0;
 }
