99d4e23d7a6ba093411b037e7e0812107a6a38a7
coolabhays <abhaysp9955@gmail.com>
2021-05-07 22:08:38 +0530

    doing proper vector comparision by comparing every element with mismatch

--- src/department.cpp
+++ src/department.cpp
@@ -8,9 +8,21 @@ namespace entities {
 	department::department(std::string name, std::vector<course>&& courses):
 		_name{name}, _courses{courses} {}
 
+	template<typename T>
+	bool is_equal(const std::vector<T>& v1, const std::vector<T>& v2) {
+		std::pair<typename std::vector<T>::const_iterator,
+			typename std::vector<T>::const_iterator> ppair = std::mismatch(
+					v1.begin(), v1.end(), v2.begin());
+		return (ppair.first == v1.cend() && ppair.second == v2.cend());
+	}
+
 	bool department::operator==(const department& other) const {
-		return ((this->_name == other.get_name())
-				&& (this->_courses == other.get_courses()));
+		bool res = true;
+		if (this->_name != other.get_name())
+			res = false;
+		if (!is_equal(this->_courses, other.get_courses()))
+			res = false;
+		return res;
 	}
 
 	std::string department::get_name() const { return this->_name; }
