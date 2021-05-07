4826d16131a218e048350592e8da7ba7641985bf
coolabhays <abhaysp9955@gmail.com>
2021-05-07 22:08:53 +0530

    just some formatting

--- src/schedule.cpp
+++ src/schedule.cpp
@@ -21,14 +21,11 @@ namespace gen_algo {
 			for (entities::course& crs: dept.get_courses()) {
 				entities::sec_class new_class(this->_sec_class_number++, dept, crs);
 				new_class.set_room(this->_gene_data.get_rooms().at(
-							util::get_random_num(this->_gene_data.get_rooms().size())
-							));
+							util::get_random_num(this->_gene_data.get_rooms().size())));
 				new_class.set_instructor(crs.get_instructors().at(
-							util::get_random_num(crs.get_instructors().size())
-							));
+							util::get_random_num(crs.get_instructors().size())));
 				new_class.set_class_time(this->_gene_data.get_class_times().at(
-							util::get_random_num(this->_gene_data.get_class_times().size())
-							));
+							util::get_random_num(this->_gene_data.get_class_times().size())));
 				this->_sec_classes.push_back(new_class);
 			}
 		}
