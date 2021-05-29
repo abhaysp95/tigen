7f335d2f1e9298a491226d9fb13c03e883e899e6
coolabhays <abhaysp9955@gmail.com>
2021-05-29 13:35:00 +0530

    added entity_not_found class for exception

--- inc/extras/extra_util.hpp
+++ inc/extras/extra_util.hpp
@@ -4,11 +4,23 @@
 #include <ctime>
 #include <cstdlib>
 #include <vector>
+#include <string>
 
 namespace util {
 	//int get_random_num(size_t upto);
 	int rngi(int limit);
 	double rngr(double limit);
+
+	class entitity_not_found: public std::exception {
+		private:
+			std::string _msg;
+		public:
+			entitity_not_found();
+			entitity_not_found(std::string& msg);
+			entitity_not_found(std::string&& msg);
+			virtual const char* what() const throw();
+	};
+
 };
 
 #endif
