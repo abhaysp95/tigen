d07f669384d7cd742388f5099f18b066c287fbfa
coolabhays <abhaysp9955@gmail.com>
2021-06-12 10:03:08 +0530

    added child demo_data, selection_data class for data parent class

--- inc/data.hpp
+++ inc/data.hpp
@@ -12,21 +12,26 @@
 #include <vector>
 #include <string>
 
+extern const size_t DEMO_DEPTS_SIZE;
+extern const size_t DEMO_COURSES_SIZE;
+extern const size_t DEMO_INSTRUCTORS_SIZE;
+extern const size_t DEMO_ROOMS_SIZE;
+extern const size_t DEMO_CLASS_TIMES_SIZE;
+
 namespace gen_algo {
-	class data {
-		private:
+	class data {   // <-- PARENT CLASS
+		//private:
+			//void create_data();
+		protected:
 			std::vector<entities::department> _depts;
 			std::vector<entities::course> _courses;
 			std::vector<entities::instructor> _instructors;
 			std::vector<entities::room> _rooms;
 			std::vector<entities::class_time> _class_times;
 			size_t _classes_count;
-			//data* _instance;
-
-			//data* get_instance();  // returning the instance as reference
-			void create_data();
 		public:
 			data();
+			void create_data();
 			// accessors
 			std::vector<entities::department> get_deparatments() const;
 			std::vector<entities::course> get_courses() const;
@@ -34,6 +39,17 @@ namespace gen_algo {
 			std::vector<entities::room> get_rooms() const;
 			std::vector<entities::class_time> get_class_times() const;
 			size_t get_classes_count() const;
+	};
+
+	class selection_data: public data {  // <-- CHILD1 CLASS
+		public:
+			explicit selection_data( const data* data );
+
+	};
+
+	class demo_data: public data {  // <-- CHILD2 CLASS
+		public:
+			explicit demo_data( const data* data );
 	};
 };
 
