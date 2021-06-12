b6d77663098c34c36c0e6f2adfc5c04f1a2b418c
coolabhays <abhaysp9955@gmail.com>
2021-06-12 10:00:40 +0530

    refined syntax

--- src/extras/extra_util.cpp
+++ src/extras/extra_util.cpp
@@ -1,28 +1,16 @@
-// cpp file for "extra/extra_util.hpp"
+// cpp file for "extras/extra_util.hpp"
 
 #include "../../inc/extras/extra_util.hpp"
-
-#include <random>
-#include <chrono>
-
 
 namespace util {
 	std::mt19937_64 rang(std::chrono::high_resolution_clock::now().time_since_epoch().count());
 
-
-	int rngi(int lim) {
-		std::uniform_int_distribution<int> uid(0, lim - 1);
-		return uid(rang);
+	int rngi( int limit ) {
+		std::uniform_int_distribution<int> uid( 0, limit - 1 );
+		return uid( rang );
 	}
-
-	double rngr(double lim) {
-		std::uniform_real_distribution<double> urd(0, lim - 1);
-		return urd(rang);
+	double rngr( double limit ) {
+		std::uniform_real_distribution<double> urd( 0, limit - 1 );
+		return urd( rang );
 	}
-
-	/*int get_random_num(size_t upto) {
-		int random = rand();
-		return (random - (random / upto) * upto);
-	}*/
-
 };
