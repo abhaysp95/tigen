99e270c0d3591255b05f95ccf1511013a24a4cc6
coolabhays <abhaysp9955@gmail.com>
2021-06-14 20:41:40 +0530

    logic for curses selection

--- inc/data.hpp
+++ inc/data.hpp
@@ -11,12 +11,31 @@
 
 #include <vector>
 #include <string>
+#include <curses.h>
+#include <menu.h>
 
 extern const size_t DEMO_DEPTS_SIZE;
 extern const size_t DEMO_COURSES_SIZE;
 extern const size_t DEMO_INSTRUCTORS_SIZE;
 extern const size_t DEMO_ROOMS_SIZE;
 extern const size_t DEMO_CLASS_TIMES_SIZE;
+
+extern const int MAX_TITLE_LEN;
+
+typedef struct _show_item {
+	char* selection;
+	char** id;
+	char** desc;
+} SHOW_ITEM;
+
+void print_title_desc();
+void print_in_middle( WINDOW* my_win, int starty, int startx, int width, std::string&& msg);
+void free_show_item( SHOW_ITEM* item_detail, int length );
+void create_menu_win( MENU** my_menu, WINDOW** my_win, int max_item_size, const char* msg = "Select Item" );
+void move_through_items( MENU* my_menu, WINDOW* my_win, SHOW_ITEM* item_detail );
+void free_menu_and_items( MENU** my_menu, ITEM*** my_items, int count );
+std::vector<std::string> get_selected_ids( char* selected_ids, const char* delim = " " );
+
 
 namespace gen_algo {
 	class data {   // <-- PARENT CLASS
@@ -31,7 +50,7 @@ namespace gen_algo {
 			data();
 			data( long get_null );
 			// accessors
-			std::vector<entities::department> get_deparatments() const;
+			std::vector<entities::department> get_departments() const;
 			std::vector<entities::course> get_courses() const;
 			std::vector<entities::instructor> get_instructors() const;
 			std::vector<entities::room> get_rooms() const;
@@ -40,6 +59,8 @@ namespace gen_algo {
 	};
 
 	class selection_data: public data {  // <-- CHILD1 CLASS
+		private:
+			void init_curses_selection( selection_data* obj );
 		public:
 			explicit selection_data( const data* data );
 
