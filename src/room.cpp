88eb35f7b364aabef09bbde6a2d78e73b911adf8
coolabhays <abhaysp9955@gmail.com>
2021-06-16 16:14:02 +0530

    operator overloaded and new constructor added

--- src/room.cpp
+++ src/room.cpp
@@ -3,15 +3,23 @@
 #include "../inc/room.hpp"
 
 namespace entities {
-	room::room() {}
-	room::room(std::string id, size_t capacity):
-		_id{ id }, _capacity{ capacity } {}
+	room::room()
+		: _id{}, _name{}, _capacity{} {  }
+	room::room(std::string id, std::string name, size_t capacity)
+		: _id{ id }, _name{ name }, _capacity{ capacity } {}
 
-	bool room::operator==(const room& other) const {
+	bool room::operator==( const room& other ) const {
 		return ((this->_id == other.get_id())
 				&& (this->_capacity == other.get_capacity()));
 	}
 
+	bool room::operator<( const room& other ) const {
+		return this->get_id() < other.get_id();
+	}
+
 	std::string room::get_id() const { return this->_id; }
+
+	std::string room::get_name() const { return this->_name; }
+
 	size_t room::get_capacity() const { return this->_capacity; }
 };
