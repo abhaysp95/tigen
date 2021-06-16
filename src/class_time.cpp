dba4fe94e1d505a457c3824b78f5f700ea033dec
coolabhays <abhaysp9955@gmail.com>
2021-06-16 16:10:40 +0530

    added constructor and extra accessor

--- src/class_time.cpp
+++ src/class_time.cpp
@@ -3,15 +3,21 @@
 #include "../inc/class_time.hpp"
 
 namespace entities {
-	class_time::class_time() {}
-	class_time::class_time(std::string id, std::string time):
-		_id{id}, _time{time} {}
+	class_time::class_time()
+		: _id{}, _time{} {  }
+	class_time::class_time(std::string id, std::string time)
+		: _id{id}, _time{time} {}
 
 	bool class_time::operator==(const class_time& other) const {
 		return ((this->_id == other.get_id())
 				&& (this->_time == other.get_time()));
 	}
 
+	bool class_time::operator<( const class_time& other ) const {
+		return this->get_id() < other.get_id();
+	}
+
 	std::string class_time::get_id() const { return this->_id; }
 	std::string class_time::get_time() const { return this->_time; }
+	std::string class_time::get_name() const { return this->_time; }
 };
